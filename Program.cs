28a026859a1e03f098f91ed6a66e756f61731d8b
Erik Berns <erik.berns@gmx.de>
2024-01-07 06:17:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+foreach (int integerYo in array)
+{
+    if (integerYo < currentSmallest)
+        currentSmallest = integerYo;
+}
+Console.WriteLine(currentSmallest);
+
+
+int total = 0;
+foreach (int totalScore in array)
+    total += totalScore;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
+
